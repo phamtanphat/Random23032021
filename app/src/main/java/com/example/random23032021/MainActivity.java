f8969de23948180c0770f5cba9307a9c933e8a25
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-04-03 20:22:11 +0700

    xử lý cho biểu thức cộng

--- app/src/main/java/com/example/random23032021/MainActivity.java
+++ app/src/main/java/com/example/random23032021/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -38,7 +39,33 @@ public class MainActivity extends AppCompatActivity {
         // Toast.makeText(this,"Thong bao",Toast.LENGTH_LONG).show();
 
         // Kiểm tra dữ liệu trong system
-        Log.d("BBB",mBtnReset.getText().toString());
+        // Log.d("BBB",mBtnReset.getText().toString());
 
+        // Luồng xử lý của chức năng
+
+        mBtnCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Lấy dữ liệu từ 2 edittext
+
+                String textSoThu1 = mEdtSoThu1.getText().toString();
+                String textSoThu2 = mEdtSoThu2.getText().toString();
+
+                if (textSoThu1.length() <= 0 || textSoThu2.length() <= 0){
+                    Toast.makeText(MainActivity.this, "Chưa nhập đủ thông tin!!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                int soThu1 = Integer.parseInt(textSoThu1);
+                int soThu2 = Integer.parseInt(textSoThu2);
+
+                int ketQua = soThu1 + soThu2;
+
+                
+                // 5 + 5 = 10
+                 mTvKetQua.setText(soThu1 + " + " + soThu2 + " = " + ketQua);
+
+            }
+        });
     }
 }
